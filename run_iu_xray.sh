572063fcbe60ca79cf7f3cba4bf965e1b7512469
stellating <38685039+stellating@users.noreply.github.com>
2021-08-20 18:06:03 +0800

    Add files via upload

---
+++ run_iu_xray.sh
@@ -0,0 +1,13 @@
+python main.py \
+--image_dir ../R2Gen-wrong/data/iu_xray/images/ \
+--ann_path ../R2Gen-wrong/data/iu_xray/new_annotation.json \
+--dataset_name iu_xray \
+--max_seq_length 60 \
+--threshold 3 \
+--batch_size 8 \
+--epochs 100 \
+--save_dir results/iu_xray \
+--step_size 50 \
+--gamma 0.1 \
+--seed 9223 \
+--resume results/iu_xray/model_best.pth
